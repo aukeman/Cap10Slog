cf486ec15fd61409b8393e50aa18f0d72a25a1eb
Jake Aukeman <jaukeman@virtualhold.com>
2014-09-15 23:34:56 -0400

    replace rtf control with data grid view

---
+++ Cap10Slog/View/LogTextView.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Cap10Slog.View
+{
+    partial class LogTextView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.rawLog = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.ColumnHeadersVisible = false;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.rawLog});
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.Size = new System.Drawing.Size(150, 150);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // rawLog
+            // 
+            this.rawLog.HeaderText = "Raw Log";
+            this.rawLog.Name = "rawLog";
+            this.rawLog.ReadOnly = true;
+            this.rawLog.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.rawLog.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // LogTextView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView);
+            this.Name = "LogTextView";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rawLog;
+
+    }
+}
---
+++ Cap10Slog/View/LogTextView.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Cap10Slog.Model;
+
+namespace Cap10Slog.View
+{
+    public partial class LogTextView : UserControl
+    {
+        public LogTextView()
+        {
+            InitializeComponent();
+        }
+
+        public LogTimelineView TimelineView
+        {
+            get;
+            set;
+        }
+
+        private LogFileCollection logFileCollection;
+        public LogFileCollection LogFileCollection
+        {
+            get { return this.logFileCollection; }
+            set
+            {
+                this.logFileCollection = value;
+
+                if (this.logFileCollection != null &&
+                    0 < this.logFileCollection.LogRecords.Length)
+                {
+//                    this.richTextBox.Text = String.Join("", this.logFileCollection.LogRecords.Select<LogRecord, string>(lr => lr.Raw).ToArray());
+
+                    this.dataGridView.DataSource = 
+                        this.logFileCollection.LogRecords.Where<LogRecord>(lr => !this.LogFileCollection.GetLogThread(lr.ThreadID).Filtered).Select<LogRecord, string>(lr => lr.Raw).ToArray();
+
+                }
+                else
+                {
+                    this.dataGridView.DataSource = new object[0];
+                }
+
+                this.Refresh();
+            }
+        }
+
+    }
+}
+;
